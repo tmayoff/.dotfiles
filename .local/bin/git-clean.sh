8384671bb4486684d55f0fc4f0fbf29bf568aeb5
Tyler Mayoff <tyler@tylermayoff.com>
2022-11-04 09:53:57 -0400

    Added things for debian package maintenance

--- .local/bin/git-clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env sh
-
-echo "Cleaning remote branches"
-git remote prune origin
-echo "Cleaning local branches"
-git for-each-ref --format '%(refname:short)' refs/heads | grep -v master | xargs git branch -D
