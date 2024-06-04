781716b38345b015ea5e014ee7157c0cfb86b853
Tyler Mayoff <tyler@tylermayoff.com>
2024-06-04 10:52:40 -0400

    Added some fish configs

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -29,6 +29,10 @@
             sha256 = "sha256-6SNXVe6EfVwcXH9O6BxNw+v4/uhKhCtVS3XE2GTc2Sc=";
       ***REMOVED***
         ***REMOVED***);
+
+        lnav = prev.lnav.overrideAttrs (old: ***REMOVED***
+          version = "0.12.2";
+        ***REMOVED***);
       ***REMOVED***)
   ***REMOVED***
 
@@ -41,7 +45,7 @@
     homeConfigurations = ***REMOVED***
       "tyler" = home-manager.lib.homeManagerConfiguration ***REMOVED***
         inherit pkgs;
-        extraSpecialArgs = ***REMOVED*** inherit unstable; ***REMOVED***;
+        extraSpecialArgs = ***REMOVED***inherit unstable;***REMOVED***;
         modules = [./home.nix];
   ***REMOVED***
 ***REMOVED***
