47b46637c3a4dd07b298328501690e73a11252d7
Tyler Mayoff <tyler@tylermayoff.com>
2024-06-14 09:29:56 -0400

    Added .local/bin to path

--- .local/bin/restic-lastbackup.sh
+++ .local/bin/restic-lastbackup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-restic -r "$BACKUP_LOCATION" snapshots | tail -3 | head -1 | awk '***REMOVED***print "Last backup:" $2 " " $3***REMOVED***'
-restic -r "$BACKUP_LOCATION" check
+restic snapshots | tail -3 | head -1 | awk '***REMOVED***print "Last backup:" $2 " " $3***REMOVED***'
+restic check
