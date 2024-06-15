3db67538c7c7b042a67d9cca3f04e07832751005
Tyler Mayoff <tyler@tylermayoff.com>
2024-06-15 09:02:51 -0400

    Added better overlay setuo with custom packages

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake utils demo";
+  description = "Home manager flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
@@ -8,48 +8,45 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    mesonlsp.url = "https://flakehub.com/f/tmayoff/nix-mesonlsp/0.1.4.tar.gz";
   };
 
   outputs = {
+    self,
     nixpkgs,
     # nix-unstable,
     home-manager,
-    flake-utils,
-    mesonlsp,
     ...
-  }: let
-    overlays = [
-      (final: prev: {
-        darkman = prev.darkman.overrideAttrs (old: {
-          src = final.fetchFromGitLab {
-            owner = "WhyNotHugo";
-            repo = "darkman";
-            rev = "main";
-            sha256 = "sha256-6SNXVe6EfVwcXH9O6BxNw+v4/uhKhCtVS3XE2GTc2Sc=";
-          };
-        });
-
-        lnav = prev.lnav.overrideAttrs (old: {
-          version = "0.12.2";
-        });
-      })
-      mesonlsp.overlay.default
-    ];
-
+  } @ inputs: let
+    inherit (self) outputs;
+    # overlays = [
+    #   import ./package.nix
+    #   (final: prev: {
+    #     darkman = prev.darkman.overrideAttrs (old: {
+    #       src = final.fetchFromGitLab {
+    #         owner = "WhyNotHugo";
+    #         repo = "darkman";
+    #         rev = "main";
+    #         sha256 = "sha256-6SNXVe6EfVwcXH9O6BxNw+v4/uhKhCtVS3XE2GTc2Sc=";
+    #       };
+    #     });
+    #     lnav = prev.lnav.overrideAttrs (old: {
+    #       version = "0.12.2";
+    #     });
+    #   })
+    #   mesonlsp.overlay.default
+    # ];
     system = "x86_64-linux";
-    pkgs = (import nixpkgs) {inherit system overlays;};
+    pkgs = (import nixpkgs) {inherit system;};
     # unstable = (import nix-unstable) {inherit system;};
   in {
+    overlays = import ./overlays {inherit inputs;};
+
     defaultPackage.${system} = home-manager.defaultPackage.${system};
 
     homeConfigurations = {
       "tyler" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        # extraSpecialArgs = {inherit unstable;};
+        extraSpecialArgs = {inherit inputs outputs;};
         modules = [./home.nix];
       };
     };
---
+++ .config/home-manager/pkgs/default.nix
@@ -0,0 +1,25 @@
+pkgs: {
+  fish-lsp = pkgs.mkYarnPackage rec {
+    pname = "fish-lsp";
+    version = "1.0.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "ndonfris";
+      repo = "fish-lsp";
+      rev = "v${version}";
+      hash = "sha256-o5WomHED/JaamHkEOHdRJHBkfbrfJ4NbvnD/1q1yYSs=";
+    };
+    offlineCache = pkgs.fetchYarnDeps {
+      yarnLock = src + "/yarn.lock";
+      hash = "sha256-hmaLWO1Sj+2VujrGD2A+COfVE2D+tCnxyojjq1512K4=";
+    };
+
+    buildPhase = ''
+      runHook preBuild
+
+      export HOME=$(mktemp -d)
+      yarn --offline compile
+      ls -la
+      runHook postBuild
+    '';
+  };
+}
