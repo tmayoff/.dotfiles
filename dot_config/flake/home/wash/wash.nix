5005128920481963efea6c62053d2dcb192c3116
Tyler Mayoff <tyler@tylermayoff.com>
2025-01-22 22:45:58 -0500

    ^

--- dot_config/flake/home/wash/wash.nix
+++ dot_config/flake/home/wash/wash.nix
@@ -1,7 +1,12 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   imports = [../common.nix ../gnome.nix ../home-backup.nix ../gui.nix];
 
   home.packages = with pkgs; [
     ventoy
+    (config.lib.nixGL.wrap renderdoc)
   ];
 }
