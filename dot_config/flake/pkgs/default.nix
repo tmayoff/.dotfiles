87bb07f015c266fe6eb8608e0bb837b52c819a12
Tyler Mayoff <tyler.mayoff@hingehealth.com>
2025-02-14 09:58:25 -0500

    Merged

--- dot_config/flake/pkgs/default.nix
+++ dot_config/flake/pkgs/default.nix
@@ -1,2 +1,9 @@
 pkgs: {
+  sbarlua = {
+    src = pkgs.fetchFromGitHub {
+      repo = "SbarLua";
+      owner = "FelixKratz";
+      rev = "437bd2031da38ccda75827cb7548e7baa4aa9978";
+    };
+  };
 }
