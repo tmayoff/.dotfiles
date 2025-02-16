6219543b45ef7ff2e457ade695abecaef5682951
Tyler Mayoff <tyler@tylermayoff.com>
2025-02-16 09:28:49 -0500

    Removed things in gui.nix

--- dot_config/flake/home/gui.nix
+++ dot_config/flake/home/gui.nix
@@ -1,6 +1,5 @@
 {
   inputs,
-  config,
   pkgs,
   ...
 }: {
@@ -22,28 +21,28 @@
     libreoffice
     obsidian
 
-    (config.lib.nixGL.wrap unstable.onagre)
+    # (config.lib.nixGL.wrap unstable.onagre)
   ];
 
   # programs.vscode.enable = true;
 
-  programs.alacritty = {
-    enable = true;
-    package = config.lib.nixGL.wrap pkgs.alacritty;
-  };
+  # programs.alacritty = {
+  #   enable = true;
+  #   package = config.lib.nixGL.wrap pkgs.alacritty;
+  # };
 
-  programs.wezterm = {
-    enable = false;
-    package = config.lib.nixGL.wrap pkgs.wezterm;
-    extraConfig = ''
-      return {
-        font_size=9.25,
-        color_scheme="catppuccin-latte",
-        hide_tab_bar_if_only_one_tab = true,
-        window_frame = {
-          font_size = 10
-        },
-      }
-    '';
-  };
+  # programs.wezterm = {
+  #   enable = false;
+  #   package = config.lib.nixGL.wrap pkgs.wezterm;
+  #   extraConfig = ''
+  #     return {
+  #       font_size=9.25,
+  #       color_scheme="catppuccin-latte",
+  #       hide_tab_bar_if_only_one_tab = true,
+  #       window_frame = {
+  #         font_size = 10
+  #       },
+  #     }
+  #   '';
+  # };
 }
