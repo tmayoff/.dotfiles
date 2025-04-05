a8dc8aafae31d148074456eb1f3544aa1d56924e
Tyler Mayoff <tyler@tylermayoff.com>
2025-04-04 20:42:16 -0400

    Removed old scripts

--- private_dot_local/bin/executable_,restic-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-restic backup "$HOME" --exclude-file "$XDG_CONFIG_HOME"/restic/ignore --exclude-caches
-restic unlock
-restic forget --keep-daily 7 --keep-weekly 4 --prune
--- private_dot_local/bin/executable_,restic-lastbackup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-restic snapshots | tail -3 | head -1 | awk '{print "Last backup: " $2 " " $3}'
-restic check
