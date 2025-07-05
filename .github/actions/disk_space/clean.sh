44a35b583bc7ddf675ea90676cccd47a5da407e7
Tyler Mayoff <tyler@tylermayoff.com>
2025-07-04 22:49:24 -0400

    remove powershell and az

--- .github/actions/disk_space/clean.sh
+++ .github/actions/disk_space/clean.sh
@@ -7,12 +7,14 @@ paths=(
   /usr/share/dotnet
   /usr/share/swift
   /usr/share/miniconda
+  /usr/share/az*
   /usr/lib/jvm
   /usr/lib/llvm-*
   /usr/lib/google-cloud-sdk
   /usr/lib/dotnet
   /usr/local/.ghcup
   /usr/local/share/chromium
+  /usr/local/share/powershell
   /usr/local/lib/android
   /usr/local/julia1.11.5
   /opt/ghc
