04dc72791f66e626a35920200efe31dec7d6f8f9
Tyler Mayoff <tyler@tylermayoff.com>
2025-07-04 22:44:42 -0400

    added more for removal

--- .github/actions/disk_space/clean.sh
+++ .github/actions/disk_space/clean.sh
@@ -10,14 +10,17 @@ paths=(
   /usr/lib/jvm
   /usr/lib/llvm-*
   /usr/lib/google-cloud-sdk
+  /usr/lib/dotnet
   /usr/local/.ghcup
   /usr/local/share/chromium
   /usr/local/lib/android
   /usr/local/julia1.11.5
   /opt/ghc
   /opt/az
+  /opt/pipx
   /opt/microsoft
   /opt/hostedtoolcache
+  /etc/skel/.rustup
 )
 
 sudo rm -rf "${paths[@]}"
