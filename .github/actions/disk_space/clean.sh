e23efc0d85e45310e281196bc99c5c51bd7c86d9
Tyler Mayoff <tyler@tylermayoff.com>
2025-07-04 22:41:27 -0400

    array expansion

--- .github/actions/disk_space/clean.sh
+++ .github/actions/disk_space/clean.sh
@@ -3,21 +3,21 @@
 set -x
 
 paths=(
-  "/home/runner/.rustup"
-  "/usr/share/dotnet"
-  "/usr/share/swift"
-  "/usr/share/miniconda"
-  "/usr/lib/jvm"
-  "/usr/lib/llvm-*"
-  "/usr/lib/google-cloud-sdk"
-  "/usr/local/.ghcup"
-  "/usr/local/share/chromium"
-  "/usr/local/lib/android"
-  "/usr/local/julia1.11.5"
-  "/opt/ghc"
-  "/opt/az"
-  "/opt/microsoft"
-  "/opt/hostedtoolcache"
+  /home/runner/.rustup
+  /usr/share/dotnet
+  /usr/share/swift
+  /usr/share/miniconda
+  /usr/lib/jvm
+  /usr/lib/llvm-*
+  /usr/lib/google-cloud-sdk
+  /usr/local/.ghcup
+  /usr/local/share/chromium
+  /usr/local/lib/android
+  /usr/local/julia1.11.5
+  /opt/ghc
+  /opt/az
+  /opt/microsoft
+  /opt/hostedtoolcache
 )
 
 sudo rm -rf "${paths[@]}"
